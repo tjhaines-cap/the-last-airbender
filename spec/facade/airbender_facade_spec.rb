cab9c724a050454a6fbb5722585ce103a5c234fb
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-22 09:47:44 -0600

    Feat: test and create airbender facade

--- spec/facade/airbender_facade_spec.rb
+++ spec/facade/airbender_facade_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe AirbenderFacade do
       fire_nation_members = AirbenderFacade.get_nation_members("Fire Nation")
 
       expect(fire_nation_members.length).to eq(97)
-      # expect(fire_nation_members).to be_all(Member)
+      expect(fire_nation_members).to be_all(Member)
     end
   end
 end
